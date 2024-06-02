9d596c5647d62b755e502127a41d03aca883484a
grinderos <grinderos.coding@gmail.com>
2024-06-02 22:37:23 +0300

    AD 4.1.3 in create config files

--- eureka-client/src/main/java/eurekaClientStart/EurekaClientApplication.java
+++ eureka-client/src/main/java/eurekaClientStart/EurekaClientApplication.java
@@ -1,9 +1,11 @@
-package appstart;
+package eurekaClientStart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
